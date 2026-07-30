76cdbe666906c7637556bd27450d8ccf84bfdeae
Manideep4005 <manideepnakka098@gmail.com>
2026-07-30 18:02:42 +0530

    Logs in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import app from "./app";
 import { env } from "./config/env";
 
+console.log("Process ID:", process.pid);
+console.log("Started at:", new Date().toISOString());
+
 app.listen(env.PORT, () => {
     console.log(`🚀 Server running on port ${env.PORT}`);
 });
