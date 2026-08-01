a0581c82766f3df1f68f701a09b677036b57e6fc
Manideep4005 <manideepnakka098@gmail.com>
2026-08-01 12:27:03 +0530

    cache related changes

--- src/app.ts
+++ src/app.ts
@@ -20,6 +20,17 @@ app.use(express.json());
 
 app.use(cookieParser());
 
+
+app.use((req, res, next) => {
+    res.setHeader(
+        "Cache-Control",
+        "no-store, no-cache, must-revalidate, proxy-revalidate"
+    );
+    res.setHeader("Pragma", "no-cache");
+    res.setHeader("Expires", "0");
+    next();
+});
+
 app.get("/", (_, res) => {
     res.json({
         success: true,
